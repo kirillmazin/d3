d1c620435374a6b807218bf059d1ad4e60b820cc
Kirill Mazin <kirill.mazin@ucop.edu>
2015-06-25 15:38:21 -0700

    Added brand fonts

--- js/models/nav_m.js
+++ js/models/nav_m.js
@@ -1,12 +1,13 @@
 define(['backbone'], function (Backbone) {
-	
+
 	var Nav_m = Backbone.Model.extend({
 		defaults:{
 			"header":"public offering",
-			"id":"id_01"
-		}	
+			"id":"id_01",
+			"display":"Public Offering"
+		}
 	});
-	
+
 	return Nav_m;
 
 });
--- js/views/nav/button_v.js
+++ js/views/nav/button_v.js
@@ -3,45 +3,46 @@ define([
 "underscore",
 "backbone",
 "text!templates/button.html"], function ($, _,Backbone,Tmpl_button) {
-	
+
 	var Button_v = Backbone.View.extend({
 		template:_.template(Tmpl_button),
-		
-		
-		
+
+
+
 		initialize:function () {
-		
-			
+
+
 			this.render();
 			this.attach_events();
-			
+
 		},
 		attach_events:function(){
-		
-			
+
+
 			this.$("#"+this.id).on("click",$.proxy(this.dotheclick,this));
-			
+
 			//this.$("#"+this.id).css("background-color","#000")
 		},
 		onmouseenter: function () {
-			
-			
+
+
 		},
 		dotheclick:function () {
-			//console.log("------" + this.model.get("header"));
+
 			var header = this.model.get("header");
 			this.trigger("update",{id:this.id, header: header});
 		},
 		render:function () {
-			
-			
-			
-			this.$el.append(this.template({title:this.model.get("header"),id:this.id}));
+
+			var title  = this.model.get("display");
+			var event_trigger = this.model.get("header");
+
+			this.$el.append(this.template({event_trigger:event_trigger, title:title,id:this.id}));
 			return this;
 		}
 	});
-	
+
 	return Button_v;
-	
+
 
 });
